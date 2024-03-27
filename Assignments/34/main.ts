3489555a05a44b6681331dccc6c2e1978562d41c
asadiqbal006 <165188753+asadiqbal006@users.noreply.github.com>
2024-03-27 23:32:11 +0500

    34 assignment done

---
+++ Assignments/34/main.ts
@@ -0,0 +1,8 @@
+let favouritePizza = ["Arabian Pizza", "Crown Pizza", "BBQ Pizza"];
+let sortFavouritePizza = favouritePizza.sort();
+
+for (let i = 0; i < sortFavouritePizza.length; i++) {
+  console.log("\nI like ", sortFavouritePizza[i]);
+}
+
+console.log("\nI really like pizza!");
